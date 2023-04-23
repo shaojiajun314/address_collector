0487f92f37ca17e86115f97ec75ab7ab586a4abb
shaojiajun314 <63770117@qq.com>
2023-04-24 02:35:41 +0800

    update

--- address_collector.py
+++ address_collector.py
@@ -1,3 +1,4 @@
+import platform
 import asyncio
 import aiohttp
 import argparse
@@ -18,8 +19,10 @@ class Gather():
 			self.thread_nums = cpu_count()
 		else:
 			self.thread_nums = thread_nums
+		# self.descriptors_limits = descriptors_limits
 
 	def run(self):
+		print(f'running.....\r\nthread: {self.thread_nums}, coroutine_nums: {self.async_nums}')
 		ts = [
 			Thread(target=self.detect)
 			for i in range(self.thread_nums)
@@ -31,14 +34,22 @@ class Gather():
 
 
 	def detect(self):
+		plat = platform.system().lower()
+		if plat == 'windows':
+		    print('windows系统')
+		    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+		else:
+		    print('linux/mac 系统')
 		loop = asyncio.new_event_loop()
 		asyncio.set_event_loop(loop)
+		# descriptors_limits_semaphore = asyncio.Semaphore(self.descriptors_limits // self.thread_nums)
 		loop.run_until_complete(asyncio.gather(
 			*[
 				asyncio.ensure_future(self.__generate_key_task(), loop=loop)
 				for i in range(self.async_nums)
 			]
 		))
+		return
 
 	@property
 	def rpc(self):
@@ -46,6 +57,7 @@ class Gather():
 
 	async def __generate_key_task(self):
 		timeout = aiohttp.ClientTimeout(total=2)
+		count = 1
 		while 1:
 			acct = Account.create()
 			try:
@@ -58,7 +70,8 @@ class Gather():
 						}
 					)
 					if r.status != 200:
-						print(f'error status {r.status}')
+						if r.status != 429:
+							print(f'error status {r.status}')
 						await asyncio.sleep(SLEEP_SECONDS)
 						continue
 					
@@ -69,7 +82,13 @@ class Gather():
 							'key': acct.key.hex()	
 						}))
 						f.close()
-					print(j)
+					# print(j)
+					if count == 10:
+						print('got 10')
+					if count == 1000000:
+						print('got 1000000')
+						count = 0
+					count += 1
 			except asyncio.exceptions.TimeoutError:
 				continue
 			except:
@@ -81,11 +100,13 @@ if __name__ == '__main__':
 	parser.add_argument('--coroutine_nums', type=int, required=True, help='协程数量')
 	parser.add_argument('--thread_nums', type=int, default=0, help='线程数量')
 	parser.add_argument('--rpcs', type=str, required=True, help='rpc 地址","分割')
+	# parser.add_argument('--descriptors_limits', type=int, required=True, help='句柄数量限制')
 	args = parser.parse_args()
 	rpcs = args.rpcs.split(',')
 	g = Gather(
 		rpcs=rpcs,
 		async_nums=args.coroutine_nums or 10,
-		thread_nums=args.thread_nums
+		thread_nums=args.thread_nums,
+		# descriptors_limits=args.descriptors_limits
 	)
 	g.run()
